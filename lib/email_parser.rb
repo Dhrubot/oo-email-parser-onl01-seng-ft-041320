5161ff3f5a3d9fdc14af703c1ea5d694eb299b53
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-26 03:21:39 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,10 +3,10 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor :parse, :email
+  attr_accessor :emails
   
-  def initialize(email)
-    @email = email
+  def initialize(emails)
+    @emails = emails
   end
   def parse
     @email.split(",")
